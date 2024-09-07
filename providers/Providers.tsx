842f4fdeb7ad1f0d2f88842cafc6db2a0ec9e1e1
othegagan <gagankumar8711@gmail.com>
2024-09-08 00:45:12 +0530

    chore: Add tanstack/react-query

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,17 +1,33 @@
 'use client';
 
-import * as React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
+import * as React from 'react';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
 
+export function TanStackProvider({ children }: { children: React.ReactNode }) {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: true,
+                staleTime: 30 * 1000,
+                gcTime: 1 * 60 * 1000,
+                retry: 2
+            }
+        }
+    });
+
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <ThemeProvider attribute='class' defaultTheme='dark' disableTransitionOnChange>
-            {children}
+            <TanStackProvider>{children}</TanStackProvider>
         </ThemeProvider>
     );
 }
